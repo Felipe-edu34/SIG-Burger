a0d3205e1aacbe5b6383816d336b9a22d1dd64a2
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-11 20:31:50 -0300

    feat: adicionando um struct ao header file

--- headers/cardapio.h
+++ headers/cardapio.h
@@ -1,6 +1,15 @@
 #ifndef CARDAPIO_H
 #define CARDAPIO_H
 
+typedef struct {
+    int codigo;
+    char nome[50];
+    char categoria[20];
+    char descricao[100];
+    float preco;
+    int disponivel;
+} Itemcardapio;
+
 void tela_menu_principal();
 void cardapio();
 
