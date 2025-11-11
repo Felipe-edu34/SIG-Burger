82f5f12436d90cbc404b6570ed842df7f84e8a79
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-11 17:19:47 -0300

    feat: adicionando a função de navegação do CRUD cardapio

--- cardapio.c
+++ cardapio.c
@@ -4,7 +4,7 @@
 
 
 void menu_cardapio() {
-    
+
     system("clear||cls");
     printf("╔══════════════════════════════════════════════════╗\n");
     printf("║                  MÓDULO CARDÁPIO                 ║\n");
@@ -20,4 +20,46 @@ void menu_cardapio() {
     printf("╚══════════════════════════════════════════════════╝\n"); 
     printf("Escolha uma opção: ");
 
+}
+
+
+
+void cardapio() {
+    int opcao;
+
+    do {
+        menu_cardapio();
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                printf("Adicionar Item ao Cardápio selecionado.\n");
+                // Chamar função para adicionar item
+                break;
+            case 2:
+                printf("Remover Item do Cardápio selecionado.\n");
+                // Chamar função para remover item
+                break;
+            case 3:
+                printf("Atualizar Item do Cardápio selecionado.\n");
+                // Chamar função para atualizar item
+                break;
+            case 4:
+                printf("Pesquisar Item do Cardápio selecionado.\n");
+                // Chamar função para pesquisar item
+                break;
+            case 5:
+                printf("Visualizar Cardápio selecionado.\n");
+                // Chamar função para visualizar cardápio
+                break;
+            case 0:
+                printf("Voltando ao Menu Principal...\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+
+        system("pause");
+
+    } while (opcao != 0);
 }
