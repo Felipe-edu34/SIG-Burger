78e250e28b0cd41b95102f1cf8bb45424c44808e
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-11 17:37:54 -0300

    feat: adicionando a função limparbuffer

---
+++ utils.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+void limpar_tela() {
+    system("clear||cls");
+}
+
+
+
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
