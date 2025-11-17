5e940d26ba45eb952d59b3ca7e6fc5e071571c4f
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-17 14:10:09 -0300

    refactor: alterando a função ler preço de void para float

--- headers/valida.h
+++ headers/valida.h
@@ -1,7 +1,7 @@
 #ifndef VALIDA_H
 #define VALIDA_H
 
-int validar_preco(float preco);
+int valida_preco(float* preco);
 int valida_nome(const char* nome);
 int valida_data(const char* data);
 int valida_categoria(const char* categoria);
