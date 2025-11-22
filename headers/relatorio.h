49ae0180f0a54fc9305db2158778e797d3a4506b
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-22 09:55:18 -0300

    refactor: adicionando algumas funções de lista dinamica para melhorar o relatorio

--- headers/relatorio.h
+++ headers/relatorio.h
@@ -3,6 +3,7 @@
 #include "estoque.h"
 
 
+
 void menu_relatorio();
 void relatorio();
 void exibindo_cardapio_por_categoria();
@@ -10,13 +11,18 @@ void relatorio_cardapio();
 void relatorio_cardapio_itens_disponiveis();
 void relatorio_cardapio_itens_indisponiveis();
 void procurar_item_por_categoria();
-void exibir_todo_o_estoque();
 void exibir_itens_com_baixa_quantidade();
 void exibir_itens_indisponiveis_estoque();
 void exibir_itens_por_nome();
 void exibindo_item_do_cardapio_por_preco();
 void listar_estoque_por_quantidade();
 void liberar_lista(Nodeproduto *lista);
+void ordenar_por_quantidade(Nodeproduto *lista);
+Nodeproduto* montar_lista_estoque();
+void listar_todo_estoque();
+void exibir_lista_estoque(Nodeproduto *lista);
+
+
 
 
 #endif // RELATORIO_H
