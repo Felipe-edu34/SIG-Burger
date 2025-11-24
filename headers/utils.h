6f344927e6c0105a0cce2dce34130fe32d92a412
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-24 15:35:43 -0300

    feat: adicionando a funçao de exclusao de acento

--- headers/utils.h
+++ headers/utils.h
@@ -7,6 +7,9 @@ void pausar();
 char* ler_string(char* destino, int tamanho);
 void tela_inicial ();
 void func_Ani(int tempo);
+int strcmp_sem_acento(const char* s1, const char* s2);
+char remover_acento(char c);
+
 
 #endif //UTILS.H
 
